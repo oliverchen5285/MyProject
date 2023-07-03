eb237a2ba9169c817dc03baf740208d6019e51cc
Oliver Chen <oliverchen5285@gmail.com>
2023-07-03 11:01:30 -0700

    added helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  3 10:50:08 2023
+
+@author: oliverchen
+"""
+
+f = open('hello.txt', 'r')
+
+contents = f.readlines()
+
+print(contents)
